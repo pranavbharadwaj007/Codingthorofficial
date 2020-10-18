56c8526b51fb2e7afb38454ab2f37fa0f1a1ab6c
Pranv <pranavbharadwaj142@gmail.com>
2020-10-18 11:30:46 +0530

    done

--- javascript/resp.js
+++ javascript/resp.js
@@ -1,18 +1,4 @@
-
-var d=new Date();
+var d = new Date();
 // let foot=document.getElementById('foot');
 // let footercon=`<small>&copy; Copyright ${d.getFullYear()}, Coding Thor</small>`;
 // foot.innerHTML=footercon;
-
-
-
-
-burger =document.querySelector('.burger');
-navbar =document.querySelector('.navbar');
-navlist =document.querySelector('.nav-list');
-rightnav =document.querySelector('.right-nav');
-burger.addEventListener('click',()=>{
-rightnav.classList.toggle('v-class-resp');
-navlist.classList.toggle('v-class-resp');
-navbar.classList.toggle('h-nav-resp');
-})
